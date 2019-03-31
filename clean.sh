cb1e5f0e613b5616656aa2fb88c077abcd60ea18
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-31 00:00:43 +0000

    Fixed clean script

--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #pio run --target clean
 
-SOURCE_FILE="src/SoilMoistureSensorCalibratedPumpUI/SoilMoistureSensorCalibratedPumpUI.ino"
+SOURCE_FILE="src/sketch/Serial1602ShieldSystemUI/Serial1602ShieldSystemUI.ino"
 
 sed -i "s/#define VERSION .*/#define VERSION \"1-0-0-0\"/" $SOURCE_FILE
