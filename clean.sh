37c8bf226aeaae7c765d73a4cf76b524d3d92c81
CompulsiveCoder <compulsivecoder@gmail.com>
2019-03-31 00:04:03 +0000

    Fixes

--- clean.sh
+++ clean.sh
@@ -1,5 +1,1 @@
-#pio run --target clean
-
-SOURCE_FILE="src/sketch/Serial1602ShieldSystemUI/Serial1602ShieldSystemUI.ino"
-
-sed -i "s/#define VERSION .*/#define VERSION \"1-0-0-0\"/" $SOURCE_FILE
+sh inject-version.sh "1-0-0-1"
--- inject-version.sh
+++ inject-version.sh
@@ -3,6 +3,6 @@ BUILD_NUMBER=$(cat buildnumber.txt)
 
 FULL_VERSION="$VERSION-$BUILD_NUMBER"
 
-SOURCE_FILE="src/SoilMoistureSensorCalibratedPumpUI/SoilMoistureSensorCalibratedPumpUI.ino"
+SOURCE_FILE="src/sketch/Serial1602ShieldSystemUI/Serial1602ShieldSystemUI.ino"
 
 sed -i "s/#define VERSION .*/#define VERSION \"$FULL_VERSION\"/" $SOURCE_FILE
